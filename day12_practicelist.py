9bfd4fa39bd6cdd2af84af82d89e7bbc9f1cb421
sakshichandrakantthombre-debug <sakshichandrakantthombre@gmail.com>
2026-07-07 23:20:48 +0530

    added the file program practice daily basic

---
+++ day12_practicelist.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+#1) define the user defined function using :
+def area_circle(r):
+    circle=3.14*r*r
+    r(2)
+print(area_circle)
+
+
+# In[18]:
+
+
+#2)Take two number and print sum
+
+a=6
+b=10
+sum=a+b
+print(sum)
+
+
+# In[21]:
+
+
+list=[1, 2, 3, 4, 5]
+print(list)
+
+
+# In[23]:
+
+
+print(max(list))
+
+
+# In[24]:
+
+
+print(min(list))
+
+
+# In[32]:
+
+
+print(type(list))
+
+list=[1, 2, 3, 4, 5]
+print(len(list))
+# In[18]:
+
+
+# create a list:
+n= ["apple", "grapes", "pineapple"]
+n.append("papaya")
+print(n)
+
+
+# In[19]:
+
+
+n= ["apple", "grapes", "pineapple"]
+n.insert(3, "123" )
+print(n)
+
+
+# In[20]:
+
+
+n= ["apple", "grapes", "pineapple"]
+n[0]="iceberry"
+print(n)
+
+
+# In[28]:
+
+
+x=[5, 6, 7, 8, 9]
+x.extend([10])
+print(x)
+
+
+# In[29]:
+
+
+x=[5, 6, 7, 8, 9]
+x.insert(6,4)
+print(x)
+
+
+# In[30]:
+
+
+list=[1, 2, 3, 4, 5]
+list[1]=7
+print(list)
+
+
+# In[ ]:
+
+
+
+
