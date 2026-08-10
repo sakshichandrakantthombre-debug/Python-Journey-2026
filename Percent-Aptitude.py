3dc61a23e40d33fcde9ce2380917bf1db7629a62
Sakshi chandrakant Thombre <sakshichandrakantthombre@gmail.com>
2026-08-10 16:27:52 +0530

    added the file of a aptitude question solve by the python programing  percentage increases and decreses find value and percetage.

---
+++ Percent-Aptitude.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # aptitude python code
+
+# In[1]:
+
+
+#A student sccored 450 marks out of 600. find the percentage.
+
+marks=450
+total=600
+percentage=(marks/total)*100
+print("Percentage:",percentage)
+
+
+# In[2]:
+
+
+# A number is increased from 400 to 500 find the percentage increase.
+old=400
+new=500
+increase=new-old
+percentage=(increase/old)*100
+print("Percentage Increase:",percentage)
+
+
+# In[3]:
+
+
+# A number is decreased from 800 to 680. find the percentage decresase.
+old=800
+new=680
+decrease=old-new
+percentage=(decrease/old)*100
+print("Percentage Decrease:", percentage)
+
+
+# In[4]:
+
+
+# A student gets 360 out of 500. find the percentage.
+marks=360
+total=500
+percentage=(marks/total)*100
+print("Percentage Increase:", percentage)
+
+
+# In[6]:
+
+
+# A price increases from 600 to 750. find the percentage decreases.
+
+old=750
+new=600
+decrease=old-new
+percentage=(decrease/old)*100
+print("Pecentage Decreases:", percentage)
+
+
+# In[11]:
+
+
+# A price increases from 1,000 to 850. find the percentage decreases.
+old=1000
+new=850
+decrease=old-new
+percentage=(decrease/old)*100
+print("Percentage Decrease:", percentage)
+
+
+
+# In[13]:
+
+
+# 25% of a numbe is 80. find the number.
+
+perct=25
+value=80
+number=value*100/perct
+print("Numbers:", number)
+
+
+# In[14]:
+
+
+# 60% of a number is 900. find the number.
+
+perct=60
+value=900
+number=value*100/perct
+print("Numbers:", number)
+
+
+# In[19]:
+
+
+# A number  is increased by 20% and becomes 240. find the original number.
+perct=20
+value=240
+orignal_num=(240*100)/120
+print("Numbers:", orignal_num)
+
+
+# In[22]:
+
+
+# cp=500, sp=600, find the loss and loss%.
+
+cp=500
+sp=600
+profit=sp-cp
+profit_Percent=(profit/cp)*100
+print(profit)
+print(profit_Percent)
+
+
+# In[ ]:
+
+
+
+
