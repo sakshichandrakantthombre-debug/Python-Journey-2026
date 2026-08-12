9f8f872217303a22a9e12a5a8f78a99fe9978ada
Sakshi chandrakant Thombre <sakshichandrakantthombre@gmail.com>
2026-08-12 18:06:01 +0530

    added the  file of python ratio and proportion.

---
+++ Ratio_Proportion.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #Ratio & Proportion.
+
+# In[1]:
+
+
+#Q1>The ratio of boys to girls is 3:2. If there are 30 boys, how many girls are there?
+
+boys=30
+girls=boys*2/3
+print(girls)
+
+
+# In[2]:
+
+
+#Q2> Divide 500 in the ratio 2:3. find both shares.
+a=500
+b=a*2/3
+print(b)
+
+
+# In[3]:
+
+
+#Q3> if 5 pens cost 100, what is the cost of 8 pens?
+cost=100
+pens=8
+answer=cost/5*pens
+print(answer)
+
+
+# In[1]:
+
+
+#Q4> two numbers are in ratio 3:5. If their sum is 64, what is the smaller number?
+
+total=64
+a,b=3,5
+smaller=total*a/(a+b)
+print(smaller)
+
+
+# In[4]:
+
+
+#5> two numbers in ration 4:7. Their difference id 18. find the smaller numbers.
+difference=18
+a,b=7,4
+smaller=difference*b/(a-b)
+print(smaller)
+
+
+# In[7]:
+
+
+#6> The ratio of boys to girls in a a class is 3:2. If there are 30 boys, how many girls are there?
+
+multi=30
+a,b=3,2
+how_many=multi/a*b
+print(how_many)
+
+
+# In[ ]:
+
+
+
+
