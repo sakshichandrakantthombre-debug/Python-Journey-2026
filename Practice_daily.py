3d64dd023e7ec6838f297d2fb407f687353e4090
Sakshi chandrakant Thombre <sakshichandrakantthombre@gmail.com>
2026-08-13 18:39:55 +0530

    added the daily practice set for revision in python journey.

---
+++ Practice_daily.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # daily aptitude python series 
+
+# In[3]:
+
+
+#1>A shopkeeper buys a pen for 100 and sells it for 120 find the profit and profit percentage.
+
+cp=100
+sp=120
+profit=120-100
+profit_cp=profit/100*100
+print(profit_cp)
+
+
+# In[5]:
+
+
+# 2>A book is purchased for 500 and sold for 450 find the loss and loss percentage.
+cp=500
+sp=450
+loss=500-450
+Loss=loss/500*100
+print(Loss)
+
+
+# In[7]:
+
+
+# 3>A mobile cover costs 200. the shopkeeper earns 25% profit.find the selling price.
+cp=200
+Profit=25
+sp=200*125/100
+print(sp)
+
+
+# In[8]:
+
+
+# 4>A article is sold for 720 at a 10% loss. find the cost price.
+sp=720
+loss=10
+cp=720*100/90
+print(cp)
+
+
+# In[11]:
+
+
+#5>A product is bought for 800 and sold for 1000. find the cost price.
+cp=800
+sp=1000
+profit=sp-cp
+profit_cost=profit/800*100
+print(profit_cost)
+
+
+# In[13]:
+
+
+#6>A  shopkeeper buys an item for 1200 and sold at 20% loss. Find Sp.
+cp=1200
+profit=15
+sp=2000*80/100
+print(sp)
+
+
+# In[15]:
+
+
+#7>An item is sold for 1500 with a profit of 25% . find sp.
+sp=1500
+profit=25
+cp=1500*100/125
+print(cp)
+
+
+# In[16]:
+
+
+#8>An item is sold for 1600 with a loss of 20%. find cp.
+sp=1600
+Loss=20
+cp=1600*100/80
+print(cp)
+
+
+# In[17]:
+
+
+#9>An item is cost price 1200 with profit 15% . finf the sp.
+cp=1200
+Profit=15
+sp=1200*115/100
+print(sp)
+
+
+# In[19]:
+
+
+# 10>An item is Sold price is 5000, and the profit 70% . find the cp,
+sp=5000
+profit=70
+cp=5000*100/170
+print(cp)
+
+
+# In[ ]:
+
+
+
+
