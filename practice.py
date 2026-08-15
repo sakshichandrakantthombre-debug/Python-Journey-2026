7322f85c356c91f902898ecd4b187521e8221af5
Sakshi chandrakant Thombre <sakshichandrakantthombre@gmail.com>
2026-08-15 21:34:32 +0530

    added the file of the basic operation are performed in the file.

---
+++ practice.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+#WAP calculate the area
+h=int(input("Enter the number"))
+b=int(input("Enter the number"))
+area_triangle=0.5*h*b
+print("calculate the area of triangle" ,area_triangle)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+#wap calculate the power of input number
+n1=int(input("Enter the number"))
+result=n1**2
+print("calculate the power" ,result)
+
+
+# In[ ]:
+
+
+
+
+
+# In[8]:
+
+
+# wap calculate the area of rectangle
+n1=float(input("Enter the number"))
+n2=float(input("Enter the number"))
+area_rec=n1*n2
+print("calculatle the area of rectangle" ,area_rec)
+
+
+# In[6]:
+
+
+#wap calculate the squreroot of input number
+n1=int(input("Enter the number"))
+result=n1**0.5
+print("calculate the squrerroot" ,result)
+
+
+# In[5]:
+
+
+a=4
+print(a**0.5)
+
+
+# In[4]:
+
+
+#wap a display the last digit of a number:
+num= int(input("Enter the number :"))
+if num>=10:
+    print("num")
+else:
+    last = num % 10
+    print(last)
+
+
+# In[9]:
+
+
+ch =input("Enter  a single character: ")
+if ch.lower():
+     print("It is Vowel")
+else:
+    print("Given number is odd")
+
+
+# In[26]:
+
+
+#wap calculate factorial of given number:
+num=int(input("Enter the number"))
+res=1
+i=1
+while(i<=num):
+    res=res*i
+    i=i+1
+    print(res)
+
+
+# In[1]:
+
+
+num=5
+res1=1
+while(num>0):
+    res1=res1*num
+    num=num-1
+    print(res1)
+
+
+# In[2]:
+
+
+num =6
+a=3
+while(num>0):
+    a=a*num
+    num=num-1
+    print(a)
+
+
+# In[3]:
+
+
+a = 10
+print(type(a))
+
+
+# In[5]:
+
+
+name="sakshi"
+print(type(name))
+
+
+# In[1]:
+
+
+a=34
+b=32
+print(a+b)
+
+
+# In[4]:
+
+
+n=[1, 2, 3, 4, 5]
+print(n)
+
+
+# In[ ]:
+
+
+
+
